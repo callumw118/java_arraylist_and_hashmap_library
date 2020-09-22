6bf310da34f3a6b425bb5b7d11f637627b97980a
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-22 18:39:53 +0100

    Creates instance of book object and sets up the test

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,10 @@
 public class Book {
+
+    private String title, author, genre;
+
+    public Book(String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Fellowship of the Ring", "J.R.R Tolkein", "Fantasy");
+    }
 }
