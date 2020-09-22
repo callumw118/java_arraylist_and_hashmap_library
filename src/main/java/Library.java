b72a7fd1647ef51e97a0d8060adfb73f997a28b7
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-22 18:50:56 +0100

    Adds a bookCount and addBook function to library and tests

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,4 +7,12 @@ public class Library {
     public Library(){
         this.books = new ArrayList<>();
     }
+
+    public int getBookCount(){
+        return this.books.size();
+    }
+
+    public void addBook(Book book){
+        this.books.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,11 +1,22 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
     private Library library;
+    private Book book1;
 
     @Before
     public void before(){
         library = new Library();
+        book1 = new Book("Fellowship of the Ring", "J.R.R Tolkein", "Fantasy");
+    }
+
+    @Test
+    public void canAddBookToLibrary(){
+        library.addBook(book1);
+        assertEquals(1, library.getBookCount());
     }
 }
